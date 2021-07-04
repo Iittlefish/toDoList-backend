fa32885ee63bd06af79b035e9cadeff309d572c4
leave3310 <sampisam3@gmail.com>
2021-07-04 19:51:40 +0800

    finish middleware

---
+++ src/controller/authentication.ts
@@ -0,0 +1,52 @@
+import * as express from "express";
+import * as jwt from "jsonwebtoken";
+import secret from '@/config/tokenSecret'
+
+export function expressAuthentication(
+    request: express.Request,
+    securityName: string,
+    scopes?: string[]
+): Promise<any> {
+    if (securityName === "api_key") {
+        let token;
+        if (request.query && request.query.access_token) {
+            token = request.query.access_token;
+        }
+
+        if (token === secret.jwtSecret) {
+            return Promise.resolve({
+                id: 1,
+                name: "Ironman",
+            });
+        } else {
+            return Promise.reject({});
+        }
+    }
+
+    if (securityName === "jwt") {
+        const token =
+            request.body.token ||
+            request.query.token ||
+            request.headers["x-access-token"];
+
+        return new Promise((resolve, reject) => {
+            if (!token) {
+                reject(new Error("No token provided"));
+            }
+            jwt.verify(token, "[secret]", function (err: any, decoded: any) {
+                if (err) {
+                    reject(err);
+                } else {
+                    // Check if JWT contains all required scopes
+                    for (let scope of scopes) {
+                        if (!decoded.scopes.includes(scope)) {
+                            reject(new Error("JWT does not contain required scope."));
+                        }
+                    }
+                    resolve(decoded);
+                }
+            });
+        });
+    }
+    return Promise.reject(new Error("No Auth"));
+}
